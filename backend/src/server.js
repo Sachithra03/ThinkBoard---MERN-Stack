d158fae06d1672d35d4ddd412ff6b8d12863ccb3
Sachithra Indrachapa <sachithrai1003@gmail.com>
2025-11-21 14:50:26 +0530

    feat: add rate limiting functionality using Upstash Redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,25 @@ import express from 'express';
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
+import ratelimit from './config/upstash.js';
+import rateLimiterl from './middleware/rateLimiter.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
-connectDB();
+
 
 //middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started on PORT: ", PORT);
+connectDB().then(()=> {  
+    app.listen(PORT, () => {
+        console.log("Server started on PORT: ", PORT);
+    });
 });
